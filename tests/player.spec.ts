360798908a4c20506057cc3c6476f2999d9e714f
Paulo Sandim <pauloedusandim@gmail.com>
2025-01-27 22:32:21 -0300

    testing music player

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -1,8 +1,16 @@
 import { test, expect } from '@playwright/test'
 
 test('deve tocar uma música', async ({ page }) => {
+
+  const song = {
+    title: 'Bughium'
+  }
+
   await page.goto('/')
 
   const loggedUser = page.locator('.logged-user')
   await expect(loggedUser).toHaveText('Fernando Papito')
+
+  await page.click('section > div > div:nth-child(1) > div > button')
+  await page.waitForTimeout(5000)
 })
