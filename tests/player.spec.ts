aff9048d6107d5fa6736a9a05a8b97f4dc73f030
Paulo Sandim <pauloedusandim@gmail.com>
2025-01-27 23:04:31 -0300

    create intercept testing

--- tests/player.spec.ts
+++ tests/player.spec.ts
@@ -3,23 +3,32 @@ import { test, expect } from '@playwright/test'
 test('deve tocar uma música', async ({ page }) => {
 
   const song = {
-    title: 'Bughium'
+    id: 1,
+    title: "Smells Like Test Script",
+    artist: "Nullvana",
+    description: "Nullvana",
+    image: "https://raw.githubusercontent.com/qaxperience/mock/main/covers/nevertesting.jpg",
+    type: "album",
+    src: "https://raw.githubusercontent.com/qaxperience/mock/main/songs/nirvana.mp3"
   }
+
+  await page.route('**/songs', route => route.fulfill({
+    status: 200,
+    body: JSON.stringify([song])
+  }))
 
   await page.goto('/')
 
   const loggedUser = page.locator('.logged-user')
   await expect(loggedUser).toHaveText('Fernando Papito')
 
-  const songCard = page.locator('.song').filter({hasText: song.title})
+  const songCard = page.locator('.song').filter({ hasText: song.title })
 
   const play = songCard.locator('.play')
   const pause = songCard.locator('.pause')
 
   await play.click()
-  await expect(pause).toBeVisible({timeout: 2000})
-  await expect(play).toBeVisible({timeout: 17100})
+  await expect(pause).toBeVisible({ timeout: 2000 })
+  await expect(play).toBeVisible({ timeout: 7000 })
 
-  // await page.click('section > div > div:nth-child(1) > div > button')
-  await page.waitForTimeout(5000)
 })
